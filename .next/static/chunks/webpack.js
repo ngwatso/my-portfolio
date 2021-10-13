064dfa1dd3043ee6f564bfb0c0f51480f980b4f8
ngwatso <ngwatson79@yahoo.com>
2021-10-13 14:28:50 -0400

    added sass components for each component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e274e3834ca156a7944b"; }
+/******/ 		__webpack_require__.h = function() { return "6df1eab2e41975d46c63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
